c6954255a9558055672bffbc7291b79bead79598
Marine Bascunana <marine.bascunana@gmail.com>
2021-04-29 20:04:52 +0200

    home page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'bootstrap';
+import "bootstrap-slider";
 
 Rails.start()
 Turbolinks.start()
